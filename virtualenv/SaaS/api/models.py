407e5b781f927d31aa22081505172facab0de0fa
Valery Petrov <valera071998@gmail.com>
2018-11-05 18:46:56 +0300

    [Add] Man base model.

--- virtualenv/SaaS/api/models.py
+++ virtualenv/SaaS/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# base classes
+class Man(models.Model):
+    name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    patronymic = models.CharField(max_length=35)
+    description = models.CharField(max_length=200)
+
+    class Meta:
+        abstract = True
