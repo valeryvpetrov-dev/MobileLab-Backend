6b17da7e4b063668748213ce994ee41bfa5465b5
Valery Petrov <valera071998@gmail.com>
2018-11-09 14:51:38 +0300

    [Change] Skill serializer (concise) model.

--- virtualenv/SaaS/api/serializers.py
+++ virtualenv/SaaS/api/serializers.py
@@ -3,25 +3,7 @@ from rest_framework import serializers
 from .models import Skill
 
 
-class SkillSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=100)
-
-    def create(self, validated_data):
-        """
-        Create Skill instance based on validated_data
-        :param validated_data: dict of validated by Serializer data which represents Skill instance
-        :return: Skill instance based on validated_data
-        """
-        return Skill.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update an existing Skill instance using given validate_data
-        :param instance: given instance of Skill to be updated
-        :param validated_data: data for updating instance
-        :return: updated Skill instance
-        """
-        instance.name = validated_data.get('name', instance.name)
-        instance.save()
-        return instance
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ('id', 'name')
