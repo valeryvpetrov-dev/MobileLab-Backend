8dc1933348d6e196eb115ad967e22dc49b7353b6
Valery Petrov <valera071998@gmail.com>
2018-11-09 19:10:55 +0300

    [Add] Student serializer model. Related skills are represented as ids.

--- virtualenv/SaaS/api/serializers.py
+++ virtualenv/SaaS/api/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from .models import Skill
+from .models import Skill, Student
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
         fields = ('id', 'name')
+
+
+class StudentSerializerSkillsID(serializers.ModelSerializer):
+    skills = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Student
+        fields = ('id', 'name', 'last_name', 'patronymic', 'description', 'skills')
