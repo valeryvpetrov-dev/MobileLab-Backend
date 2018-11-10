49fe36c7fcf153090757602d1cb394400d3e1c98
Valery Petrov <valera071998@gmail.com>
2018-11-10 14:55:58 +0300

    [Add] Tests for SkillSerializer.

---
+++ virtualenv/SaaS/api/test/models/skill.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+
+from SaaS.api.models import Skill
+
+
+# Create your tests here.
+class TestSkill(TestCase):
+    """
+    CRUD operations of Skill model
+
+    Mention: Tests that require a database (namely, model tests)
+    will not use your “real” (production) database.
+    Separate, blank databases are created for the tests.
+    """
+    def setUp(self):
+        Skill.objects.create(name='Programming')
+
+    def test_create_skill(self):
+        skill = Skill.objects.create(name='Management')
+        self.assertIsNotNone(skill)
+
+    def test_read_skill(self):
+        skill = Skill.objects.get(name__exact='Programming')
+        self.assertIsNotNone(skill)
+
+    def test_update_skill(self):
+        new_name = 'Python programming'
+        old_skill = Skill.objects.get(name__exact='Programming')
+        old_skill_id = old_skill.id
+
+        Skill.objects.filter(name__exact='Programming').update(name=new_name)
+        new_skill = Skill.objects.get(id=old_skill_id)
+        self.assertEqual(new_skill.name, new_name)
+
+    def test_delete_skill(self):
+        Skill.objects.get(name__exact='Programming').delete()
+        with self.assertRaises(Skill.DoesNotExist):
+            Skill.objects.get(name__exact='Programming')
---
+++ virtualenv/SaaS/api/test/serializers/skill.py
@@ -0,0 +1,47 @@
+from django.test import TestCase
+
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+
+import io
+
+from ...serializers import SkillSerializer
+from ...models import Skill
+
+
+class TestSkillSerializer(TestCase):
+    """
+    CRU operations of Skill serializer model
+
+    Mention: Read, Update operations are performed by id.
+             Delete operation doesn't require serialization.
+    """
+    def setUp(self):
+        skill = Skill.objects.create(name='Serializers test')
+        self.skill_id = skill.id   # RUD
+
+    def test_create_skill(self):    # input - json
+        skill_json = b'{"name":"Writing tests"}'    # bytes
+        stream = io.BytesIO(skill_json)
+        data = JSONParser().parse(stream)   # returns dict
+        serializer = SkillSerializer(data=data)
+        if serializer.is_valid():
+            skill = serializer.save()
+            self.assertIsNotNone(skill.id)
+
+    def test_read_skill(self):  # output - json
+        skill = Skill.objects.get(pk=self.skill_id)
+        serializer = SkillSerializer(skill)
+        json = JSONRenderer().render(serializer.data)
+        self.assertIsNotNone(json)
+
+    def test_update_skill(self):    # input - json
+        new_name = "Writing tests Django"
+        skill_json = b'{"name": "Writing tests Django"}'
+        stream = io.BytesIO(skill_json)
+        data = JSONParser().parse(stream)
+        serializer = SkillSerializer(data=data)
+        if serializer.is_valid():
+            skill = Skill.objects.get(pk=self.skill_id) # skill to update
+            skill = serializer.update(skill, serializer.validated_data)
+            self.assertEqual(skill.name, new_name)
