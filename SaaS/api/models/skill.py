f25545737055c710567ba44ac72de418f02f9862
Valery Petrov <valera071998@gmail.com>
2018-12-12 22:17:38 +0300

    [Fix] Skill level field dropped.

--- SaaS/api/models/skill.py
+++ SaaS/api/models/skill.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Skill(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
 
     class Meta:
         db_table = "Skill"
