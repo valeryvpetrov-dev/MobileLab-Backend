2a2b46da19e6bcdb0ad96be646391ea831daf669
Valery Petrov <valera071998@gmail.com>
2018-12-12 13:52:29 +0300

    [Add] Skill model. Level field.

--- SaaS/api/models/skill.py
+++ SaaS/api/models/skill.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Skill(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(100), MinValueValidator(0)])
 
     class Meta:
         db_table = "Skill"
